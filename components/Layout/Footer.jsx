bf71126dcccf3d366cd79b1d4afd26659070f1a1
RxRzZ <104535415+RxRzZ@users.noreply.github.com>
2025-04-27 17:56:04 +0300

    Delete components directory

--- components/Layout/Footer.jsx
+++
@@ -1,9 +0,0 @@
-import '../../styles/components/footer.css';
-
-export default function Footer() {
-    return (
-      <footer className="app-footer">
-        <p>© {new Date().getFullYear()} TaskMaster - A simple task management app</p>
-      </footer>
-    );
-  }
